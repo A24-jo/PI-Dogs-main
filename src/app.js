4b8c25370a21c595887a2d369e8b5075fc6d7721
A24-jo <bravo24ramosjose@gmail.com>
2023-09-17 18:41:32 -0500

    backdog

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://pi-dogs-main-client-1xr5-bodgxvi7a-a24-jo.vercel.app'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', 'https://pi-dogs-main-client-1xr5-pyhs3d8m7-a24-jo.vercel.app'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
@@ -33,3 +33,4 @@ server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
 });
 
 module.exports = server;
+  
